db05a105ae06ccc3e1bb726a4ab8eb35e14135eb
syphrose <syphrose.wabulwenyi@student.moringaschool.com>
2022-06-18 19:18:04 +0300

    commit tree 6aa9a8c75704d5cf2665bb6eb581a1465e64df3c

--- main.js
+++ main.js
@@ -2,9 +2,44 @@
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
+
 // Your JavaScript code goes here!
 
+const modal = document.querySelector("#modal");
+const hearts = document.querySelectorAll(".like-glyph");
+modal.classList.add("hidden"); 
+for(let i=0; i< hearts.length; i++) {
+  hearts[i].addEventListener("click", (e) =>{
+    let heartParent = e.target;
+    heartEvent(heartParent);
+    console.log(heartParent)
 
+  });
+}
+function heartEvent(heartParent) {
+  mimicServerCall()
+    .then(function(response){
+      heartClicked(heartParent);
+      console.log("Success!");
+      console.log(response);
+    })
+
+    .catch(function(error) {
+      modal.classList.remove("hidden");
+      console.log(error);
+    });
+
+}
+function heartClicked(heartParent) {
+  if (heartParent.innerHTML === EMPTY_HEART) {
+    heartParent.classList.add("activated-heart");
+    heartParent.innerHTML = FULL_HEART;
+    console.log("Success!");
+  } else {
+    heartParent.classList.remove("activated-heart");
+    heartParent.innerHTML = EMPTY_HEART;
+  }
+}
 
 
 //------------------------------------------------------------------------------
